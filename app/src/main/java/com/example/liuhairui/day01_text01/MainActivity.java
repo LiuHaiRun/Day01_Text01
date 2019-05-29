937edbe2c6889ee905f8e3978d13c7f60b5a3409
LiuHaiRun <1014884337@qq.com>
2019-05-29 16:45:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/liuhairui/day01_text01/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.liuhairui.day01_text01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button mBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+
+    private void initView() {
+        mBtn = (Button) findViewById(R.id.btn);
+        mBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.btn:
+                break;
+        }
+    }
+}
